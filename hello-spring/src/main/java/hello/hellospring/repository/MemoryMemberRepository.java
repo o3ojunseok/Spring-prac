38d834f48493f341c098b04deb6e6661f299e632
o3ojunseok <o3ojunseok@gmail.com>
2023-04-21 08:49:42 +0900

    회원 도메인과 리포지토리만들기 (예제용)

---
+++ hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,34 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>(); // 예제라 간단하게 쓰는거임
+    private static Long sequence = 0L; // 동시성 문제 고려해야하지만 예제니까
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id)); // optional null
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny(); //람다 사용해서 필터링 파라미터와 맞는지
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
