23ec882a48af0ec5006f61302aa1475bdec8bdc5
o3ojunseok <o3ojunseok@gmail.com>
2023-05-02 09:13:18 +0900

    member post,list

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,30 +1,30 @@
-package hello.hellospring;
-
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
-import hello.hellospring.service.MemberService;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-// 자바 코드로 직접 빈 작성
-@Configuration
-public class SpringConfig {
-
-    @Bean
-    public MemberService memberService() {
-        return new MemberService(memberRepository());
-    }
-
-    @Bean
-    public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
-}
-
-// Controller 어노테이션은 그대로 둔다. 단 서비스 레포지토리 애노테이션은 제거한 상태여야 함.
-
-// 빈등록 방법 차이
-// 자바코드로 설정하지 않고 과거에는 XML문서로 했지만, 지금은 거의 하지 않는다.
-// DI에는 필드 주입 setter주입, 생성자 주입 3가지 방법이 있는데, 필드 주입은 좋지 않다. 뭔가 바꿀수 있는 방법이 없음.
-// setter injection 방식의 단점은 누군가 컨트롤러를 호출하면 퍼블릭으로 열려 있어야 한다. -> 퍼블릭 노출 단점
-// 생성자를 통해 주입하면 처음부터 조립 및 세팅이되는 시점에 생성자가 처음 들어오고 끝난다. -> 권장 방식
+//package hello.hellospring;
+//
+//import hello.hellospring.repository.MemberRepository;
+//import hello.hellospring.repository.MemoryMemberRepository;
+//import hello.hellospring.service.MemberService;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//// 자바 코드로 직접 빈 작성
+//@Configuration
+//public class SpringConfig {
+//
+//    @Bean
+//    public MemberService memberService() {
+//        return new MemberService(memberRepository());
+//    }
+//
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
+//}
+//
+//// Controller 어노테이션은 그대로 둔다. 단 서비스 레포지토리 애노테이션은 제거한 상태여야 함.
+//
+//// 빈등록 방법 차이
+//// 자바코드로 설정하지 않고 과거에는 XML문서로 했지만, 지금은 거의 하지 않는다.
+//// DI에는 필드 주입 setter주입, 생성자 주입 3가지 방법이 있는데, 필드 주입은 좋지 않다. 뭔가 바꿀수 있는 방법이 없음.
+//// setter injection 방식의 단점은 누군가 컨트롤러를 호출하면 퍼블릭으로 열려 있어야 한다. -> 퍼블릭 노출 단점
+//// 생성자를 통해 주입하면 처음부터 조립 및 세팅이되는 시점에 생성자가 처음 들어오고 끝난다. -> 권장 방식
--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller // 컨트롤러를 보고 스프링이 뜰때 컨트롤러의 객체를 생성해 스프링이 들고 있는다. -> 스프링 빈이 관리 된다.
 public class MemberController {
@@ -15,5 +21,27 @@ public class MemberController {
     @Autowired // 연결해주는 역할 어노테이션 -> 스프링 컨테이너에서 해당하는걸 가져온다.
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
